45a7a1f8031473a573a19f44b12b67e256248a08
Danila56 <danila.00.10@mail.ru>
2017-10-30 14:02:13 +0200

    heroku_first

---
+++ src/main/java/com/examplespringboot/controller/Controller.java
@@ -0,0 +1,11 @@
+package com.examplespringboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+    @GetMapping("/")
+    public String main(){
+        return "index";
+    }
+}
